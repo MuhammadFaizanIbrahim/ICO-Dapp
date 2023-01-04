16de40e8e649d0d84bb54595f27c7e0e95a75e1d
Faizan <mfaizanibrahim25@gmail.com>
2023-01-04 14:49:33 +0500

    Ok

---
+++ hardhat-tutorial/hardhat.config.js
@@ -0,0 +1,24 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+module.exports = {
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {
+    },
+    goerli: {
+      url: "https://boldest-wider-layer.ethereum-goerli.discover.quiknode.pro/0447a5232535ba5077476c817766ab32a2cbdde8",
+      accounts: ["28f5ea1b74dda607d1b080b28614266cc66176c7c35b9a09c3a0a6e75c284c41"],
+}
+  },
+  solidity: {
+    version: "0.8.7",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+ 
+}
